4385b1f94bd7a0c8b10f0b2ef2ec3ad7b0967cb3
loxt <emannuelloxt@hotmail.com>
2022-03-24 15:41:23 -0400

    feat(entity): create anemic customer entity

---
+++ src/entity/customer.ts
@@ -0,0 +1,31 @@
+class Customer {
+  constructor(
+    private _id: string,
+    private _name: string,
+    private _address: string,
+  ) {}
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get address(): string {
+    return this._address;
+  }
+
+  set address(value: string) {
+    this._address = value;
+  }
+}
