b65e34e20c4ec6eb9ca68e8dba17caca86a24ce1
loxt <emannuelloxt@hotmail.com>
2022-04-02 16:47:16 -0400

    feat(): jest config for sequelize

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,5 @@
 import {Sequelize} from 'sequelize-typescript';
+import ProductModel from '../db/sequelize/model/product.model';
 
 describe('Product repository unit tests', function() {
   let sequelize: Sequelize;
@@ -10,6 +11,7 @@ describe('Product repository unit tests', function() {
       logging: false,
       sync: {force: true},
     });
+    sequelize.addModels([ProductModel]);
     await sequelize.sync();
   });
 
