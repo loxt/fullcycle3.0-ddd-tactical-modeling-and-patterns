16f9e80831db9f9676870c8b0dc357dcf54785ce
loxt <emannuelloxt@hotmail.com>
2022-04-10 14:22:02 -0400

    feat(infra): add modules with sequelize

--- src/infrastructure/order/repository/sequelize/order.repository.ts
+++ src/infrastructure/order/repository/sequelize/order.repository.ts
@@ -1,8 +1,8 @@
-import OrderRepositoryInterface from '../../domain/checkout/repository/order-repository.interface';
-import Order from '../../domain/checkout/entity/order';
-import OrderModel from '../db/sequelize/model/order.model';
-import OrderItemModel from '../db/sequelize/model/order-item.model';
-import OrderItem from '../../domain/checkout/entity/order-item';
+import OrderRepositoryInterface from '../../../../domain/checkout/repository/order-repository.interface';
+import Order from '../../../../domain/checkout/entity/order';
+import OrderModel from './order.model';
+import OrderItemModel from './order-item.model';
+import OrderItem from '../../../../domain/checkout/entity/order-item';
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
